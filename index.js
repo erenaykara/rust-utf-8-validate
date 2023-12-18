9b5854f8b09bc493d67b8fc8d547610033fa5cf0
Will Parsons <entrocraft@gmail.com>
2023-12-18 07:12:08 +0000

    Adding operations to pass more statistical tests.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const entro_shift = function(entropy) {
   entropy ^= entropy >> 14
   entropy ^= entropy << 13
   entropy = ~(entropy ^ 1111111111)
+  entropy -= entropy << 5
   entropy = (entropy << 31) + (~entropy >> 1)
   entropy += entropy << 3
   entropy ^= 1111111111
-  entropy ^= entropy << 5
+  entropy -= entropy << 5
   return (entropy >> 1) ^ entropy
 }
 
