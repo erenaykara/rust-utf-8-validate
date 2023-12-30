1ebac3d3e91fb096db8f40bc66fd32022e22952b
Will Parsons <entrocraft@gmail.com>
2023-12-30 01:43:51 +0000

    Adding the exact production algorithm to encourage sales from evaluation.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const entro_shift = function(entropy) {
-  entropy ^= (entropy << 14) ^ (entropy >> 13)
-  entropy = ~(entropy ^ 1111111111) - (~entropy << 5)
-  entropy = (entropy << 31) + (~entropy >> 1)
-  entropy += entropy << 3
+  entropy ^= (entropy << 14) ^ (~entropy >> 13)
+  entropy = ~(entropy ^ 1111111111) - (entropy << 5)
+  entropy = (entropy << 31) + (entropy >> 1)
   entropy += entropy << 3
   entropy ^= 1111111111
   entropy -= ~entropy << 5
